6bf0f79ff53b28ccc3293bf2899fba2ef3eb67c5
TetyanaWeb <tetyana.razumova@gmail.com>
2021-09-23 11:17:27 -0400

    Initial commit with all my work javascripting  so far

---
+++ numbers.js
@@ -0,0 +1,3 @@
+const example = 123456789
+integer = 123456789;
+console.log(integer);
---
+++ tinkering.js
@@ -0,0 +1,6 @@
+function multiply(a, b) {
+  return a * b;
+}
+const result = multiply(3, 5);
+
+console.log(result);
